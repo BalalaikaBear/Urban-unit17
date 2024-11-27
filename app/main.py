ade6f9382288b6c1b85dd1f71e51072dee8c4be0
BalalaikaBear <eightes@yandex.ru>
2024-11-27 14:26:54 +0300

    None

--- app/main.py
+++ app/main.py
@@ -3,7 +3,17 @@ from routers import task, user
 
 app = FastAPI()
 
-# fastapi dev app/main.py - запуск файла
+#$ fastapi dev app/main.py - запуск файла
+
+# подготовка миграции
+# 1. $ alembic init app/migrations  - создание папки миграции
+# 2. в файле alembic.ini прописать: sqlalchemy.url = sqlite:///taskmanager.db
+# 3. в файле env.py прописать:  from app.backend.db import Base
+#                               from app.models.task import Task
+#                               from app.models.user import User
+#                               target_metadata = Base.metadata
+# 4. $ alembic revision --autogenerate -m "Initial migration"  - генерация базы данных
+# 5. $ alembic upgrade head  - применение последней миграции и создание таблиц User, Task и запись текущей версии миграции
 
 @app.get("/")
 async def welcome():
